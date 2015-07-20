726e715497b62cc771a8872fcfe14807bcde1f90
Alessandro Gaz <alessandro.gaz@cern.ch>
2015-07-20 11:57:28 +0200

    committed selection steering files and skimming at gen level tools

--- steering_files/SelectPhiK0_ch2.py
+++ steering_files/SelectPhiK0_ch2.py
@@ -20,7 +20,7 @@
 import sys
 
 if len(sys.argv) != 3:
-    sys.exit('\n Usage: basf2 SelectPhiK0_ch1.py `action` `sample`.\n\n Where `action` = `simple`, `train`, or `expert`\n and `sample` = `signal`, `BBbar` or `continuum`\n'
+    sys.exit('\n Usage: basf2 SelectPhiK0_ch2.py `action` `sample`.\n\n Where `action` = `simple`, `train`, or `expert`\n and `sample` = `signal`, `BBbar` or `continuum`\n'
     )
 
 from basf2 import *
@@ -34,15 +34,14 @@ outFile = ''
 
 # set the input files
 
-filelistSIG = ['../saved_rootfiles/B0_Phi-KK_Ks-pi+pi-_gsim-BKGx1-50000-1.root']
-#filelistCC = ['../saved_rootfiles/continuum_gsim-BKGx1-50000-1.root']
+filelistSIG = ['../saved_rootfiles/B0_Phi-KK_Ks-pi0pi0_gsim-BKGx1-50000-*.root']
+filelistCC = ['../saved_rootfiles/continuum_gsim-BKGx1-50000-1.root']
 filelistBBbar = ['../saved_rootfiles/reconstructedBBbar-10M.root']
-filelistCC = ['../saved_rootfiles/reconstructedContinuum-10M.root']
 
 
 # define input and output files
 
-outFile = 'B0_PhiK0_ch1_output.root'
+outFile = 'B0_PhiK0_ch2_output.root'
 
 if action == 'training':
     inputMdstList(filelistSIG+filelistCC)
@@ -51,26 +50,26 @@ else:
     if sample == 'signal':
         inputMdstList(filelistSIG)
         if action == 'simple':
-            outFile = 'B0_PhiK0_ch1_output_signal.root'
+            outFile = 'B0_PhiK0_ch2_output_signal.root'
         elif action == 'expert':
-            outFile = 'B0_PhiK0_ch1_output_cs_signal.root'
+            outFile = 'B0_PhiK0_ch2_output_cs_signal.root'
             
     elif sample == 'BBbar':
-        inputMdstList(filelistBBbar)
+        inputMdstList(filelistCC)
         if action == 'simple':
-            outFile = 'B0_PhiK0_ch1_output_BBbar.root'
+            outFile = 'B0_PhiK0_ch2_output_BBbar.root'
         elif action == 'expert':
-            outFile = 'B0_PhiK0_ch1_output_cs_BBbar.root'
+            outFile = 'B0_PhiK0_ch2_output_cs_BBbar.root'
             
     elif sample == 'continuum':
         inputMdstList(filelistCC)
         if action == 'simple':
-            outFile = 'B0_PhiK0_ch1_output_continuum.root'
+            outFile = 'B0_PhiK0_ch2_output_continuum.root'
         elif action == 'expert':
-            outFile = 'B0_PhiK0_ch1_output_cs_continuum.root'
+            outFile = 'B0_PhiK0_ch2_output_cs_continuum.root'
         
     else:
-        sys.exit('Input sample does not match any of the availible samples: `signal`, `BBBar` or `continuum`.'
+        sys.exit('Input sample does not match any of the availible samples: `signal`, `BBbar` or `continuum`.'
                  )
 
 
@@ -86,34 +85,33 @@ fillParticleLists([photons, electrons, muons, pions, kaons, protons])
 
 # start reconstructing the candidates
 
-fillParticleList('K_S0:mdst','0.3 < M < 0.7')
-vertexKFit('K_S0:mdst', 0.0)
-applyCuts('K_S0:mdst','0.475 < M < 0.520')
-matchMCTruth('K_S0:mdst')
-
 cutAndCopyList('gamma:good','gamma:all','0.060 < E < 6.000 and -150 < clusterTiming < 0 and clusterE9E25 > 0.75')
 calibratePhotonEnergy('gamma:good', 0.0064)
 
-reconstructDecay('pi0:all -> gamma:good gamma:good',' 0.080 < M < 0.200')
+reconstructDecay('pi0:all -> gamma:good gamma:good','0.080 < M < 0.200')
 massKFit('pi0:all',0.0)
 matchMCTruth('pi0:all')
+
+reconstructDecay('K_S0:neu -> pi0:all pi0:all', '0.4 < M < 0.6')
+massKFit('K_S0:neu',0.0)
+matchMCTruth('K_S0:neu')
 
 reconstructDecay('phi:all -> K-:all K+:all','M < 1.1')
 vertexKFit('phi:all', 0.0)
 matchMCTruth('phi:all')
 
-reconstructDecay('B0:ch1 -> phi:all K_S0:mdst','Mbc > 5.2 and abs(deltaE) < 0.2')
-vertexRave('B0:ch1', 0.0, 'B0:ch1 -> [phi -> ^K+ ^K-] K_S0')
-matchMCTruth('B0:ch1')
+reconstructDecay('B0:ch2 -> phi:all K_S0:neu','Mbc > 5.0 and abs(deltaE) < 0.5')
+vertexRave('B0:ch2', 0.0, 'B0:ch2 -> [phi -> ^K+ ^K-] K_S0')
+matchMCTruth('B0:ch2')
 
 # get the rest of the event:
-buildRestOfEvent('B0:ch1')
+buildRestOfEvent('B0:ch2')
 
 # get tag vertex ('breco' is the type of MC association)
-TagV('B0:ch1', 'breco')
+TagV('B0:ch2', 'breco')
 
 # get continuum suppression (needed for flavor tagging)
-buildContinuumSuppression('B0:ch1')
+buildContinuumSuppression('B0:ch2')
 
 
 if action == 'training':
@@ -158,11 +156,11 @@ if action == 'training':
 
     # TMVA training/testing
     teacher = register_module('TMVATeacher')
-    teacher.param('prefix', 'B0_PhiKs_ch1_TMVA')
+    teacher.param('prefix', 'B0_PhiKs_ch2_TMVA')
     teacher.param('methods', methods)
     teacher.param('variables', variables)
     teacher.param('target', 'isContinuumEvent')
-    teacher.param('listNames', ['B0:ch1'])
+    teacher.param('listNames', ['B0:ch2'])
     teacher.param('workingDirectory', 'training')
     analysis_main.add_module(teacher)
 
@@ -173,9 +171,9 @@ if action == 'expert':
 
     for method in methods:
         expert = register_module('TMVAExpert')
-        expert.param('prefix', 'B0_PhiKs_ch1_TMVA')
+        expert.param('prefix', 'B0_PhiKs_ch2_TMVA')
         expert.param('method', method)
-        expert.param('listNames', ['B0:ch1'])
+        expert.param('listNames', ['B0:ch2'])
         expert.param('expertOutputName', method + '_Probability')
         expert.param('workingDirectory', 'training')
         analysis_main.add_module(expert)
@@ -203,7 +201,7 @@ if action == 'expert':
 # with tracks, neutrals, pi0's and intermediate resonances
 # are meant only for debugging
 #
-# for the real analysis, anything included in B0_ch1
+# for the real analysis, anything included in B0_ch2
 # should be sufficient
 
 toolsTrk = ['EventMetaData', 'pi+:all']
@@ -226,9 +224,9 @@ toolsPi0 += ['MCKinematics', '^pi0:all -> ^gamma ^gamma']
 toolsPi0 += ['MCTruth', '^pi0:all -> ^gamma ^gamma']
 toolsPi0 += ['MCHierarchy', '^pi0:all -> ^gamma ^gamma']
 
-toolsKsChg = ['InvMass', '^K_S0:mdst -> pi+ pi-']
-toolsKsChg += ['Vertex', '^K_S0:mdst -> pi+ pi-']
-toolsKsChg += ['EventMetaData', '^K_S0:mdst']
+toolsKsNeu = ['InvMass', '^K_S0:neu -> ^pi0 ^pi0']
+toolsKsNeu += ['Vertex', '^K_S0:neu -> ^pi0 ^pi0']
+toolsKsNeu += ['EventMetaData', '^K_S0']
 
 toolsPhiKK = ['InvMass', '^phi -> K+ K-']
 toolsPhiKK += ['Vertex', '^phi -> K+ K-']
@@ -241,36 +239,37 @@ toolsPhiKK += ['DeltaLogL',                'phi -> ^K- ^K+']
 toolsPhiKK += ['Track',                    'phi -> ^K- ^K+']
 toolsPhiKK += ['CustomFloats[isSignal:M]', '^phi']
 
-toolsBsigCh1 = ['EventMetaData', '^B0:ch1']
-toolsBsigCh1 += ['InvMass', '^B0:ch1 -> [^phi -> K+ K-] ^K_S0']
-toolsBsigCh1 += ['Charge', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['Kinematics', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['CMSKinematics', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['DeltaEMbc', '^B0:ch1']
-toolsBsigCh1 += ['PID', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['Track', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['TrackHits', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['MCTruth', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['MCHierarchy', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['MCKinematics', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['Vertex', '^B0:ch1 -> ^phi ^K_S0']
-toolsBsigCh1 += ['MCVertex', '^B0:ch1 -> ^phi ^K_S0']
-toolsBsigCh1 += ['TagVertex', '^B0:ch1']
-toolsBsigCh1 += ['MCTagVertex', '^B0:ch1']
-toolsBsigCh1 += ['DeltaT', '^B0:ch1']
-toolsBsigCh1 += ['MCDeltaT', '^B0:ch1']
-toolsBsigCh1 += ['CustomFloats[isSignal:isContinuumEvent]', '^B0:ch1']
+toolsBsigCh2 = ['EventMetaData', '^B0:ch2']
+toolsBsigCh2 += ['InvMass', '^B0:ch2 -> [^phi -> K+ K-] K_S0']
+toolsBsigCh2 += ['InvMass[BeforeFit]', 'B0:ch2 -> [phi -> K+ K-] [^K_S0 -> ^pi0 ^pi0]']
+toolsBsigCh2 += ['Charge', 'B0:ch2 -> [phi -> ^K+ ^K-] [K_S0 -> pi0 pi0]']
+toolsBsigCh2 += ['Kinematics', '^B0:ch2 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi0 ^pi0]']
+toolsBsigCh2 += ['CMSKinematics', '^B0:ch2 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi0 ^pi0]']
+toolsBsigCh2 += ['DeltaEMbc', '^B0:ch2']
+toolsBsigCh2 += ['PID', 'B0:ch2 -> [phi -> ^K+ ^K-] [K_S0 -> pi0 pi0]']
+toolsBsigCh2 += ['Track', 'B0:ch2 -> [phi -> ^K+ ^K-] [K_S0 -> pi0 pi0]']
+toolsBsigCh2 += ['TrackHits', 'B0:ch2 -> [phi -> ^K+ ^K-] [K_S0 -> pi0 pi0]']
+toolsBsigCh2 += ['MCTruth', '^B0:ch2 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi0 ^pi0]']
+toolsBsigCh2 += ['MCHierarchy', '^B0:ch2 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi0 ^pi0]']
+toolsBsigCh2 += ['MCKinematics', '^B0:ch2 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi0 ^pi0]']
+toolsBsigCh2 += ['Vertex', '^B0:ch2 -> ^phi ^K_S0']
+toolsBsigCh2 += ['MCVertex', '^B0:ch2 -> ^phi ^K_S0']
+toolsBsigCh2 += ['TagVertex', '^B0:ch2']
+toolsBsigCh2 += ['MCTagVertex', '^B0:ch2']
+toolsBsigCh2 += ['DeltaT', '^B0:ch2']
+toolsBsigCh2 += ['MCDeltaT', '^B0:ch2']
+toolsBsigCh2 += ['CustomFloats[isSignal:isContinuumEvent]', '^B0:ch2']
 
 if action == 'expert':
-    toolsBsigCh1 += ['ContinuumSuppression', '^B0:ch1']
-    toolsBsigCh1 += ['ContinuumSuppression[FS1]', '^B0:ch1']
-    toolsBsigCh1 += ['CustomFloats[' + ':'.join(networkOutput) + ']', '^B0:ch1']
-    toolsBsigCh1 += ['CustomFloats[' + ':'.join(transformedNetworkOutputFBDT) + ']', '^B0:ch1']
-    #toolsBsigCh1 += ['CustomFloats[' + ':'.join(transformedNetworkOutputNB) + ']', '^B0:ch1']
+    toolsBsigCh2 += ['ContinuumSuppression', '^B0:ch2']
+    toolsBsigCh2 += ['ContinuumSuppression[FS1]', '^B0:ch2']
+    toolsBsigCh2 += ['CustomFloats[' + ':'.join(networkOutput) + ']', '^B0:ch2']
+    toolsBsigCh2 += ['CustomFloats[' + ':'.join(transformedNetworkOutputFBDT) + ']', '^B0:ch2']
+    #toolsBsigCh2 += ['CustomFloats[' + ':'.join(transformedNetworkOutputNB) + ']', '^B0:ch2']
 
-#toolsBsigCh1 += ['FlavorTagging', '^B0:ch1']
+#toolsBsigCh2 += ['FlavorTagging', '^B0:ch2']
 
-toolsRS = ['RecoStats', '^B0:ch1']
+toolsRS = ['RecoStats', '^B0:ch2']
 
 
 # save stuff to root file
@@ -279,10 +278,10 @@ ntupleFile(outFile)
 ntupleTree('Trks', 'pi+:all', toolsTrk)
 ntupleTree('Neu', 'gamma:all', toolsNeu)
 ntupleTree('Pi0', 'pi0:all', toolsPi0)
-ntupleTree('KsChg', 'K_S0:mdst', toolsKsChg)
+ntupleTree('KsNeu', 'K_S0:neu', toolsKsNeu)
 ntupleTree('PhiKK', 'phi:all', toolsPhiKK)
-ntupleTree('B0_ch1', 'B0:ch1', toolsBsigCh1)
-ntupleTree('RecoStats', 'B0:ch1', toolsRS)
+ntupleTree('B0_ch2', 'B0:ch2', toolsBsigCh2)
+ntupleTree('RecoStats', 'B0:ch2', toolsRS)
 
 # Process the events
 process(analysis_main)
--- steering_files/SelectPhiK0_ch3.py
+++ steering_files/SelectPhiK0_ch3.py
@@ -20,7 +20,7 @@
 import sys
 
 if len(sys.argv) != 3:
-    sys.exit('\n Usage: basf2 SelectPhiK0_ch1.py `action` `sample`.\n\n Where `action` = `simple`, `train`, or `expert`\n and `sample` = `signal`, `BBbar` or `continuum`\n'
+    sys.exit('\n Usage: basf2 SelectPhiK0_ch3.py `action` `sample`.\n\n Where `action` = `simple`, `train`, or `expert`\n and `sample` = `signal`, `BBbar` or `continuum`\n'
     )
 
 from basf2 import *
@@ -34,15 +34,14 @@ outFile = ''
 
 # set the input files
 
-filelistSIG = ['../saved_rootfiles/B0_Phi-KK_Ks-pi+pi-_gsim-BKGx1-50000-1.root']
-#filelistCC = ['../saved_rootfiles/continuum_gsim-BKGx1-50000-1.root']
+filelistSIG = ['../saved_rootfiles/B0_phi-pi+pi-pi0_KS-pi+pi-_gsim-BKGx1-50000-1.root']
 filelistBBbar = ['../saved_rootfiles/reconstructedBBbar-10M.root']
 filelistCC = ['../saved_rootfiles/reconstructedContinuum-10M.root']
 
 
 # define input and output files
 
-outFile = 'B0_PhiK0_ch1_output.root'
+outFile = 'B0_PhiK0_ch3_output.root'
 
 if action == 'training':
     inputMdstList(filelistSIG+filelistCC)
@@ -51,23 +50,23 @@ else:
     if sample == 'signal':
         inputMdstList(filelistSIG)
         if action == 'simple':
-            outFile = 'B0_PhiK0_ch1_output_signal.root'
+            outFile = 'B0_PhiK0_ch3_output_signal.root'
         elif action == 'expert':
-            outFile = 'B0_PhiK0_ch1_output_cs_signal.root'
+            outFile = 'B0_PhiK0_ch3_output_cs_signal.root'
             
     elif sample == 'BBbar':
         inputMdstList(filelistBBbar)
         if action == 'simple':
-            outFile = 'B0_PhiK0_ch1_output_BBbar.root'
+            outFile = 'B0_PhiK0_ch3_output_BBbar.root'
         elif action == 'expert':
-            outFile = 'B0_PhiK0_ch1_output_cs_BBbar.root'
+            outFile = 'B0_PhiK0_ch3_output_cs_BBbar.root'
             
     elif sample == 'continuum':
         inputMdstList(filelistCC)
         if action == 'simple':
-            outFile = 'B0_PhiK0_ch1_output_continuum.root'
+            outFile = 'B0_PhiK0_ch3_output_continuum.root'
         elif action == 'expert':
-            outFile = 'B0_PhiK0_ch1_output_cs_continuum.root'
+            outFile = 'B0_PhiK0_ch3_output_cs_continuum.root'
         
     else:
         sys.exit('Input sample does not match any of the availible samples: `signal`, `BBBar` or `continuum`.'
@@ -98,22 +97,22 @@ reconstructDecay('pi0:all -> gamma:good gamma:good',' 0.080 < M < 0.200')
 massKFit('pi0:all',0.0)
 matchMCTruth('pi0:all')
 
-reconstructDecay('phi:all -> K-:all K+:all','M < 1.1')
+reconstructDecay('phi:all -> pi0:all pi+:all pi-:all','0.95< M < 1.07')
 vertexKFit('phi:all', 0.0)
 matchMCTruth('phi:all')
 
-reconstructDecay('B0:ch1 -> phi:all K_S0:mdst','Mbc > 5.2 and abs(deltaE) < 0.2')
-vertexRave('B0:ch1', 0.0, 'B0:ch1 -> [phi -> ^K+ ^K-] K_S0')
-matchMCTruth('B0:ch1')
+reconstructDecay('B0:ch3 -> phi:all K_S0:mdst','Mbc > 5.2 and abs(deltaE) < 0.2')
+vertexRave('B0:ch3', 0.0, 'B0:ch3 -> [phi -> pi0 ^pi+ ^pi-] K_S0')
+matchMCTruth('B0:ch3')
 
 # get the rest of the event:
-buildRestOfEvent('B0:ch1')
+buildRestOfEvent('B0:ch3')
 
 # get tag vertex ('breco' is the type of MC association)
-TagV('B0:ch1', 'breco')
+TagV('B0:ch3', 'breco')
 
 # get continuum suppression (needed for flavor tagging)
-buildContinuumSuppression('B0:ch1')
+buildContinuumSuppression('B0:ch3')
 
 
 if action == 'training':
@@ -158,11 +157,11 @@ if action == 'training':
 
     # TMVA training/testing
     teacher = register_module('TMVATeacher')
-    teacher.param('prefix', 'B0_PhiKs_ch1_TMVA')
+    teacher.param('prefix', 'B0_PhiKs_ch3_TMVA')
     teacher.param('methods', methods)
     teacher.param('variables', variables)
     teacher.param('target', 'isContinuumEvent')
-    teacher.param('listNames', ['B0:ch1'])
+    teacher.param('listNames', ['B0:ch3'])
     teacher.param('workingDirectory', 'training')
     analysis_main.add_module(teacher)
 
@@ -173,9 +172,9 @@ if action == 'expert':
 
     for method in methods:
         expert = register_module('TMVAExpert')
-        expert.param('prefix', 'B0_PhiKs_ch1_TMVA')
+        expert.param('prefix', 'B0_PhiKs_ch3_TMVA')
         expert.param('method', method)
-        expert.param('listNames', ['B0:ch1'])
+        expert.param('listNames', ['B0:ch3'])
         expert.param('expertOutputName', method + '_Probability')
         expert.param('workingDirectory', 'training')
         analysis_main.add_module(expert)
@@ -203,7 +202,7 @@ if action == 'expert':
 # with tracks, neutrals, pi0's and intermediate resonances
 # are meant only for debugging
 #
-# for the real analysis, anything included in B0_ch1
+# for the real analysis, anything included in B0_ch3
 # should be sufficient
 
 toolsTrk = ['EventMetaData', 'pi+:all']
@@ -230,47 +229,47 @@ toolsKsChg = ['InvMass', '^K_S0:mdst -> pi+ pi-']
 toolsKsChg += ['Vertex', '^K_S0:mdst -> pi+ pi-']
 toolsKsChg += ['EventMetaData', '^K_S0:mdst']
 
-toolsPhiKK = ['InvMass', '^phi -> K+ K-']
-toolsPhiKK += ['Vertex', '^phi -> K+ K-']
-toolsPhiKK += ['EventMetaData', '^phi']
-toolsPhiKK += ['Kinematics',               '^phi -> ^K- ^K+']
-toolsPhiKK += ['MCTruth',                  '^phi']
-toolsPhiKK += ['Vertex',                   '^phi']
-toolsPhiKK += ['MCReconstructible',        'phi -> ^K- ^K+']
-toolsPhiKK += ['DeltaLogL',                'phi -> ^K- ^K+']
-toolsPhiKK += ['Track',                    'phi -> ^K- ^K+']
-toolsPhiKK += ['CustomFloats[isSignal:M]', '^phi']
+toolsPhi3Pi = ['InvMass', '^phi -> ^pi0 pi+ pi-']
+toolsPhi3Pi += ['Vertex', '^phi -> pi0 pi+ pi-']
+toolsPhi3Pi += ['EventMetaData', '^phi']
+toolsPhi3Pi += ['Kinematics',               '^phi -> ^pi0 ^pi+ ^pi-']
+toolsPhi3Pi += ['MCTruth',                  '^phi']
+toolsPhi3Pi += ['Vertex',                   '^phi']
+toolsPhi3Pi += ['MCReconstructible',        'phi -> ^pi0 ^pi+ ^pi-']
+toolsPhi3Pi += ['DeltaLogL',                'phi -> ^pi0 ^pi+ ^pi-']
+toolsPhi3Pi += ['Track',                    'phi -> ^pi0 ^pi+ ^pi-']
+toolsPhi3Pi += ['CustomFloats[isSignal:M]', '^phi']
 
-toolsBsigCh1 = ['EventMetaData', '^B0:ch1']
-toolsBsigCh1 += ['InvMass', '^B0:ch1 -> [^phi -> K+ K-] ^K_S0']
-toolsBsigCh1 += ['Charge', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['Kinematics', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['CMSKinematics', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['DeltaEMbc', '^B0:ch1']
-toolsBsigCh1 += ['PID', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['Track', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['TrackHits', 'B0:ch1 -> [phi -> ^K+ ^K-] [K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['MCTruth', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['MCHierarchy', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['MCKinematics', '^B0:ch1 -> [^phi -> ^K+ ^K-] [^K_S0 -> ^pi+ ^pi-]']
-toolsBsigCh1 += ['Vertex', '^B0:ch1 -> ^phi ^K_S0']
-toolsBsigCh1 += ['MCVertex', '^B0:ch1 -> ^phi ^K_S0']
-toolsBsigCh1 += ['TagVertex', '^B0:ch1']
-toolsBsigCh1 += ['MCTagVertex', '^B0:ch1']
-toolsBsigCh1 += ['DeltaT', '^B0:ch1']
-toolsBsigCh1 += ['MCDeltaT', '^B0:ch1']
-toolsBsigCh1 += ['CustomFloats[isSignal:isContinuumEvent]', '^B0:ch1']
+toolsBsigCh3 = ['EventMetaData', '^B0:ch3']
+toolsBsigCh3 += ['InvMass', '^B0:ch3 -> [^phi -> ^pi0 pi+ pi-] ^K_S0']
+toolsBsigCh3 += ['Charge', 'B0:ch3 -> [phi -> pi0 ^pi+ ^pi-] [K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['Kinematics', '^B0:ch3 -> [^phi -> ^pi0 ^pi+ ^pi-] [^K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['CMSKinematics', '^B0:ch3 -> [^phi -> ^pi0 ^pi+ ^pi-] [^K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['DeltaEMbc', '^B0:ch3']
+toolsBsigCh3 += ['PID', 'B0:ch3 -> [phi -> pi0 ^pi+ ^pi-] [K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['Track', 'B0:ch3 -> [phi -> pi0 ^pi+ ^pi-] [K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['TrackHits', 'B0:ch3 -> [phi -> pi0 ^pi+ ^pi-] [K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['MCTruth', '^B0:ch3 -> [^phi -> ^pi0 ^pi+ ^pi-] [^K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['MCHierarchy', '^B0:ch3 -> [^phi -> ^pi0 ^pi+ ^pi-] [^K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['MCKinematics', '^B0:ch3 -> [^phi -> ^pi0 ^pi+ ^pi-] [^K_S0 -> ^pi+ ^pi-]']
+toolsBsigCh3 += ['Vertex', '^B0:ch3 -> ^phi ^K_S0']
+toolsBsigCh3 += ['MCVertex', '^B0:ch3 -> ^phi ^K_S0']
+toolsBsigCh3 += ['TagVertex', '^B0:ch3']
+toolsBsigCh3 += ['MCTagVertex', '^B0:ch3']
+toolsBsigCh3 += ['DeltaT', '^B0:ch3']
+toolsBsigCh3 += ['MCDeltaT', '^B0:ch3']
+toolsBsigCh3 += ['CustomFloats[isSignal:isContinuumEvent]', '^B0:ch3']
 
 if action == 'expert':
-    toolsBsigCh1 += ['ContinuumSuppression', '^B0:ch1']
-    toolsBsigCh1 += ['ContinuumSuppression[FS1]', '^B0:ch1']
-    toolsBsigCh1 += ['CustomFloats[' + ':'.join(networkOutput) + ']', '^B0:ch1']
-    toolsBsigCh1 += ['CustomFloats[' + ':'.join(transformedNetworkOutputFBDT) + ']', '^B0:ch1']
-    #toolsBsigCh1 += ['CustomFloats[' + ':'.join(transformedNetworkOutputNB) + ']', '^B0:ch1']
+    toolsBsigCh3 += ['ContinuumSuppression', '^B0:ch3']
+    toolsBsigCh3 += ['ContinuumSuppression[FS1]', '^B0:ch3']
+    toolsBsigCh3 += ['CustomFloats[' + ':'.join(networkOutput) + ']', '^B0:ch3']
+    toolsBsigCh3 += ['CustomFloats[' + ':'.join(transformedNetworkOutputFBDT) + ']', '^B0:ch3']
+    #toolsBsigCh3 += ['CustomFloats[' + ':'.join(transformedNetworkOutputNB) + ']', '^B0:ch3']
 
-#toolsBsigCh1 += ['FlavorTagging', '^B0:ch1']
+#toolsBsigCh3 += ['FlavorTagging', '^B0:ch3']
 
-toolsRS = ['RecoStats', '^B0:ch1']
+toolsRS = ['RecoStats', '^B0:ch3']
 
 
 # save stuff to root file
@@ -280,9 +279,9 @@ ntupleTree('Trks', 'pi+:all', toolsTrk)
 ntupleTree('Neu', 'gamma:all', toolsNeu)
 ntupleTree('Pi0', 'pi0:all', toolsPi0)
 ntupleTree('KsChg', 'K_S0:mdst', toolsKsChg)
-ntupleTree('PhiKK', 'phi:all', toolsPhiKK)
-ntupleTree('B0_ch1', 'B0:ch1', toolsBsigCh1)
-ntupleTree('RecoStats', 'B0:ch1', toolsRS)
+ntupleTree('Phi3Pi', 'phi:all', toolsPhi3Pi)
+ntupleTree('B0_ch3', 'B0:ch3', toolsBsigCh3)
+ntupleTree('RecoStats', 'B0:ch3', toolsRS)
 
 # Process the events
 process(analysis_main)
