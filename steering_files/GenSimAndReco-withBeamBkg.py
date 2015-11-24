01c49ea8c94fe47e88319d071b09fadc8c36e778
alegaz <gaz@ccw21.cc.kek.jp>
2015-11-25 01:22:29 +0900

    removed glob from GenSimAndReco-withBeamBkg.py

--- steering_files/GenSimAndReco-withBeamBkg.py
+++ steering_files/GenSimAndReco-withBeamBkg.py
@@ -16,13 +16,15 @@ from simulation import add_simulation
 from reconstruction import add_reconstruction
 from reconstruction import add_mdst_output
 from ROOT import Belle2
-import glob
 
 # generate signal MC
 generateY4S(1000, Belle2.FileSystem.findFile('../dec_files/B0_phi-K+K-_KS-pi+pi-.dec'))
 
+bkgdir = '/sw/belle2/bkg/'
+bkgFiles = [bkgdir + '*.root']
+
 # simulation
-add_simulation(analysis_main, None, bkgfiles=glob.glob('/sw/belle2/bkg/*.root'))     # WITH beam background
+add_simulation(analysis_main, None, bkgFiles)     # WITH beam background
 
 # reconstruction
 add_reconstruction(analysis_main)
